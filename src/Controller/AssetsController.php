35d5c28e48f34e68fd48695f5fd0989419f5ecdc
MarquandT <thibault@marquand.pro>
2021-06-14 18:03:19 +0200

    Assets registery

--- src/Controller/AssetsController.php
+++ src/Controller/AssetsController.php
@@ -10,15 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 class AssetsController extends AbstractController {
     public function css() {
         $webpath = $this->getParameter('kernel.project_dir') . '/public/';
-        $files = [
-            'font/recursive.css',
-            'css/bootstrap.min.css',
-            'css/retro.css',
-            'css/menu.css',
-            'css/custom.css',
-            'fontawesome/css/all.min.css',
-            'css/prism.css',
-        ];
+        $files = ['font/recursive.css', 'css/bootstrap.min.css', 'css/retro.css', 'css/menu.css', 'css/custom.css', 'fontawesome/css/all.min.css', 'css/prism.css'];
         $minifier = new CSS();
         foreach ($files as $file):
             $minifier->add($webpath.$file);
@@ -27,11 +19,7 @@ class AssetsController extends AbstractController {
     }
     public function js() {
         $webpath = $this->getParameter('kernel.project_dir') . '/public/';
-        $files = [
-            'js/jquery.min.js',
-            'js/bootstrap.min.js',
-            'js/app.js',
-        ];
+        $files = ['js/jquery.min.js', 'js/bootstrap.min.js', 'js/app.js'];
         $minifier = new JS();
         foreach ($files as $file):
             $minifier->add($webpath.$file);
